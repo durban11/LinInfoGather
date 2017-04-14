e682e8de5cacb703ec8556472d374a921513f0c5
durban11 <danau@ieee.org>
2017-04-14 15:30:58 -0400

    Create layoftheland.sh

---
+++ layoftheland.sh
@@ -0,0 +1,12 @@
+::  Dana Urban
+::  Get the lay of the land
+::
+::  What is the OS:
+echo os:
+cat /etc/*-release
+:: archetecture
+uname -i
+:: and everything else
+uname -a
+:: for Debian based OS try this should fail in RHEL land
+lsb_release -a
